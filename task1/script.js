62c873dc78ea74814ecad972703876410ebaf6e4
Vaishnavi Mogal <89105831+VaishnaviMogal@users.noreply.github.com>
2021-09-15 15:47:53 +0530

    Add files via upload

---
+++ task1/script.js
@@ -0,0 +1,9 @@
+
+// video project section
+function videoslider(links) {
+  document.querySelector(".slider1").src = links;
+}
+// image slider section
+function imageslider(links) {
+  document.querySelector(".slider2").src = links;
+}
